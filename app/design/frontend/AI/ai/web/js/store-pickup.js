b07955f924e4e19c8317b17c768cd88c8ee5ebb2
luanaportellarodriguez <l.portella.rodriguez@accenture.com>
2025-10-09 17:40:50 -0300

    feat(widget): widget de seleção de retirada em loja criado

---
+++ app/design/frontend/AI/ai/web/js/store-pickup.js
@@ -0,0 +1,52 @@
+define([
+    'jquery',
+    'jquery/ui',
+    'mage/cookies'
+], function ($) {
+    'use strict';
+
+    $.widget('store.pickup', {
+        _create: function () {
+            var self = this;
+            
+            // Load saved store from sessionStorage
+            var savedStore = sessionStorage.getItem('selectedStore');
+            if (savedStore) {
+                $('.store-radio[value="' + savedStore + '"]').prop('checked', true);
+            }
+
+            // Handle store selection
+            $('.store-radio').on('change', function() {
+                var storeId = $(this).val();
+                var storeData = $(this).data('store');
+                
+                // Save to sessionStorage
+                sessionStorage.setItem('selectedStore', storeId);
+                
+                // You can also save to cookie if needed for server-side use
+                $.cookie('selected_store', storeId, { path: '/' });
+                
+                // Optional: Show feedback to user
+                self.showStoreSelected(storeData.name);
+            });
+        },
+        
+        showStoreSelected: function(storeName) {
+            // Optional: Add visual feedback when a store is selected
+            var message = $('<div class="message success">' + 
+                           '<div>Loja selecionada: ' + storeName + '</div>' +
+                           '</div>');
+            
+            $('.store-pickup-widget').prepend(message);
+            
+            // Remove message after 3 seconds
+            setTimeout(function() {
+                message.fadeOut(500, function() {
+                    $(this).remove();
+                });
+            }, 3000);
+        }
+    });
+
+    return $.store.pickup;
+});
